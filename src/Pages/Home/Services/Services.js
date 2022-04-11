f156eed731dabdd70de02320c160a8a227c73b4c
shanto360 <shanto.360@yahoo.com>
2022-04-11 22:43:10 +0600

    jetBrains-Mono-font

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const services =[
+    {id: 1, name: 'oil change', price: 100, description: ''}
+]
+const Services = () => {
+    return (
+        <div>
+            <h2>services</h2>
+        </div>
+    );
+};
+
+export default Services;
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p><small>&copy; Copyright </small></p>
+        </footer>
+    );
+};
+
+export default Footer;
